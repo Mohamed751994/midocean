3dfdbf1e754732483d77f20a367c3deab766e833
Mohamed said <msaid@taptype.co>
2024-07-14 16:58:59 +0300

    Generate Models

---
+++ database/migrations/2024_07_14_122421_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('post_id')->unsigned();
+            $table->text('comment');
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onCascade('delete');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
